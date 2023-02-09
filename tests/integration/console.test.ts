9062e019920281308e333416fc43a500cb2e4eb7
Diego Candido <diegocan.sou@gmail.com>
2023-02-08 22:23:54 -0300

    test: create tests for games and console routes

---
+++ tests/integration/console.test.ts
@@ -0,0 +1,78 @@
+import { cleanDb } from "../helpers";
+import app from "../../src/app";
+import supertest from "supertest";
+import { createConsole } from "../factories/console-factory";
+import { faker } from "@faker-js/faker";
+import httpStatus from "http-status";
+
+beforeEach(async () => {
+ await cleanDb();
+});
+
+const server = supertest(app);
+
+describe("GET /consoles", () => {
+ it("Should response with all consoles", async () => {
+  const console = await createConsole();
+
+  const response = await server.get("/consoles");
+
+  expect(response.body).toEqual([
+   {
+    id: console.id,
+    name: console.name,
+   },
+  ]);
+ });
+});
+
+describe("GET /consoles/:id", () => {
+ it("Should response with status 404 when doesnt exist a console for given id", async () => {
+  const console = await createConsole();
+
+  const response = await server.get("/consoles/0");
+
+  expect(response.status).toBe(404);
+ });
+
+ it("Should response with console data for given id", async () => {
+  const console = await createConsole();
+
+  const response = await server.get(`/consoles/${console.id}`);
+
+  expect(response.body).toEqual(console);
+ });
+});
+
+describe("POST /consoles", () => {
+ it("Should response with status 422 when given body is not valid", async () => {
+  const body = { [faker.lorem.word()]: faker.lorem.word() };
+
+  const response = await server.post("/consoles").send(body);
+
+  expect(response.status).toBe(422);
+ });
+
+ describe("when body is valid", () => {
+  it("Should response with status 409 when console already exist", async () => {
+   const console = await createConsole();
+   const body = {
+    name: console.name
+   };
+
+   const response = await server.post("/consoles").send(body);
+
+   expect(response.status).toBe(409);
+  });
+
+  it("Should response with status 201 when console created", async () => {
+   const body = {
+   name: faker.company.name()
+   };
+
+   const response = await server.post("/consoles").send(body);
+
+   expect(response.status).toBe(httpStatus.CREATED);
+  });
+ });
+});
---
+++ tests/integration/games.test.ts
@@ -0,0 +1,102 @@
+import { cleanDb } from "../helpers";
+import app from "../../src/app";
+import supertest from "supertest";
+import { createConsole } from "../factories/console-factory";
+import { createGame } from "../factories/games-factory";
+import { faker } from "@faker-js/faker";
+import httpStatus from "http-status";
+
+beforeEach(async () => {
+ await cleanDb();
+});
+
+const server = supertest(app);
+
+describe("GET /games", () => {
+ it("Should response with all games", async () => {
+  const console = await createConsole();
+  const game = await createGame(console.id);
+
+  const response = await server.get("/games");
+
+  expect(response.body).toEqual([
+   {
+    id: game.id,
+    title: game.title,
+    consoleId: game.consoleId,
+    Console: {
+     id: console.id,
+     name: console.name,
+    },
+   },
+  ]);
+ });
+});
+
+describe("GET /games/:id", () => {
+ it("Should response with status 404 when doesnt exist a game for given id", async () => {
+  const console = await createConsole();
+  const game = await createGame(console.id);
+
+  const response = await server.get("/games/0");
+
+  expect(response.status).toBe(404);
+ });
+
+ it("Should response with game data for given id", async () => {
+  const console = await createConsole();
+  const game = await createGame(console.id);
+
+  const response = await server.get(`/games/${game.id}`);
+
+  expect(response.body).toEqual(game);
+ });
+});
+
+describe("POST /games", () => {
+ it("Should response with status 422 when given body is not valid", async () => {
+  const body = { [faker.lorem.word()]: faker.lorem.word() };
+
+  const response = await server.post("/games").send(body);
+
+  expect(response.status).toBe(422);
+ });
+
+ describe("when body is valid", () => {
+  it("Should response with status 409 when game already exist", async () => {
+   const console = await createConsole();
+   const game = await createGame(console.id);
+   const body = {
+    title: game.title,
+    consoleId: game.consoleId,
+   };
+
+   const response = await server.post("/games").send(body);
+
+   expect(response.status).toBe(409);
+  });
+
+  it("Should response with status 409 when given consoleId doesnt exist", async () => {
+   const body = {
+    title: faker.company.name(),
+    consoleId: 0,
+   };
+
+   const response = await server.post("/games").send(body);
+
+   expect(response.status).toBe(409);
+  });
+
+  it("Should response with status 201 when game created", async () => {
+   const console = await createConsole();
+   const body = {
+    title: faker.company.name(),
+    consoleId: console.id,
+   };
+
+   const response = await server.post("/games").send(body);
+
+   expect(response.status).toBe(httpStatus.CREATED);
+  });
+ });
+});
